60ed89a3a76f959b2f4d82e42a708c3a6b1e3c21
Troy <yortus@gmail.com>
2026-03-09 21:18:00 +0800

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import { spritesheetPlugin } from './scripts/vite-plugin-spritesheet';
 
 export default defineConfig({
+    base: process.env.BASE_URL ?? '/',
     plugins: [spritesheetPlugin()],
 });
