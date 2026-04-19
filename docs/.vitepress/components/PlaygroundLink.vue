10de9b722617ecd92ba5e7d1666030d01a7f6a1f
Troy Gerwien <yortus@gmail.com>
2026-04-19 22:01:14 +0800

    fix broken links

--- docs/.vitepress/components/PlaygroundLink.vue
+++ docs/.vitepress/components/PlaygroundLink.vue
@@ -2,14 +2,8 @@
 declare const __SITE_ROOT__: string;
 const props = defineProps<{ preset: string; label?: string }>();
 const href = __SITE_ROOT__ + 'playground/#preset=' + props.preset;
-
-/** Force a full page navigation so VitePress's SPA router does not intercept the click. */
-function navigate(e: MouseEvent) {
-    e.preventDefault();
-    window.location.href = href;
-}
 </script>
 
 <template>
-    <a :href="href" @click="navigate">{{ label || 'Open in Playground' }}</a>
+    <a :href="href" target="_self">{{ label || 'Open in Playground' }}</a>
 </template>
